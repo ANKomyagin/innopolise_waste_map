5160b6d37bf3f4f809e529854d8f9921170b6846
ANKomyagin <144670657+ANKomyagin@users.noreply.github.com>
2026-03-22 14:49:09 +0300

    hj

--- app/main.py
+++ app/main.py
@@ -2,11 +2,12 @@
 from fastapi import FastAPI
 from app.services.sensor_pipeline import SensorProcessingPipeline
 from app.infrastructure.routing.yandex_router import DummyRoutingProvider
-from app.infrastructure.telegram.notifier import TelegramNotifier # Возвращаем
+# from app.infrastructure.telegram.notifier import TelegramNotifier  # Пока отключаем
 from app.domain.models import WebhookPayload
+from app.core.interfaces import NotificationService # Добавили импорт интерфейса
 
-from app.infrastructure.database.database import engine, Base
-from app.infrastructure.database.postgres_repo import PostgresContainerRepo
+from infrastructure.database.database import engine, Base
+from infrastructure.database.postgres_repo import PostgresContainerRepo
 
 # Создаем таблицы в БД при старте
 Base.metadata.create_all(bind=engine)
@@ -14,15 +15,23 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Innopolis Smart Waste API")
 
 # ==========================================
+# 🛠 ЗАГЛУШКА ДЛЯ УВЕДОМЛЕНИЙ (Dummy)
+# ==========================================
+class DummyNotifier(NotificationService):
+    def send_alert(self, message: str, role: str):
+        # Вместо отправки реального сообщения в ТГ, просто красиво выводим в логи Docker
+        print(f"🔔 [DUMMY ALERT | Роль: {role}] -> {message}")
+
+# ==========================================
 # 🧱 СБОРКА ПАЙПЛАЙНА
 # ==========================================
 db_repo = PostgresContainerRepo()
-notifier = TelegramNotifier() # Возвращаем
+notifier = DummyNotifier() # ИСПОЛЬЗУЕМ НАШУ ЗАГЛУШКУ
 routing_provider = DummyRoutingProvider()
 
 sensor_pipeline = SensorProcessingPipeline(
     repo=db_repo,
-    notifier=notifier, # Передаем аргумент!
+    notifier=notifier,
     enable_alerts=True
 )
 
@@ -39,6 +48,7 @@ async def get_optimal_route():
 
     route = routing_provider.build_route(origin="Депо", waypoints=to_collect)
     return {"route": route}
+
 
 @app.post("/api/sensors/webhook")
 async def receive_sensor_data(payload: WebhookPayload):
