e7d35c958fe9cb51c3b84d3519d8482fc880570f
ANKomyagin <144670657+ANKomyagin@users.noreply.github.com>
2026-03-22 14:44:44 +0300

    ghj

--- main.py
+++ main.py
@@ -2,31 +2,38 @@
 from fastapi import FastAPI
 from app.services.sensor_pipeline import SensorProcessingPipeline
 from app.infrastructure.routing.yandex_router import DummyRoutingProvider
-#from app.infrastructure.telegram.notifier import TelegramNotifier
+# from app.infrastructure.telegram.notifier import TelegramNotifier  # Пока отключаем
 from app.domain.models import WebhookPayload
+from app.core.interfaces import NotificationService # Добавили импорт интерфейса
 
-# Импортируем нашу новую БД
 from app.infrastructure.database.database import engine, Base
 from app.infrastructure.database.postgres_repo import PostgresContainerRepo
 
-# Создаем таблицы в БД при старте (если их нет)
+# Создаем таблицы в БД при старте
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Innopolis Smart Waste API")
 
 # ==========================================
-# 🧱 СБОРКА ПАЙПЛАЙНА (Dependency Injection)
+# 🛠 ЗАГЛУШКА ДЛЯ УВЕДОМЛЕНИЙ (Dummy)
 # ==========================================
-db_repo = PostgresContainerRepo()  # <-- ИСПОЛЬЗУЕМ НАСТОЯЩУЮ БД!
-#notifier = TelegramNotifier()
+class DummyNotifier(NotificationService):
+    def send_alert(self, message: str, role: str):
+        # Вместо отправки реального сообщения в ТГ, просто красиво выводим в логи Docker
+        print(f"🔔 [DUMMY ALERT | Роль: {role}] -> {message}")
+
+# ==========================================
+# 🧱 СБОРКА ПАЙПЛАЙНА
+# ==========================================
+db_repo = PostgresContainerRepo()
+notifier = DummyNotifier() # ИСПОЛЬЗУЕМ НАШУ ЗАГЛУШКУ
 routing_provider = DummyRoutingProvider()
 
 sensor_pipeline = SensorProcessingPipeline(
     repo=db_repo,
-    #notifier=notifier,
+    notifier=notifier,
     enable_alerts=True
 )
-
 
 # ==========================================
 # 🚀 API ENDPOINTS
@@ -34,7 +41,6 @@ sensor_pipeline = SensorProcessingPipeline(
 @app.get("/api/logistics/route")
 async def get_optimal_route():
     containers = db_repo.get_all()
-    # Берем координаты контейнеров, заполненных >= 70%
     to_collect = [c.coords for c in containers if c.sensor_data and c.sensor_data.fill_percent >= 70]
 
     if not to_collect:
@@ -46,6 +52,5 @@ async def get_optimal_route():
 
 @app.post("/api/sensors/webhook")
 async def receive_sensor_data(payload: WebhookPayload):
-    # Теперь передаем весь payload в пайплайн
     sensor_pipeline.process_new_data(payload)
     return {"status": "ok", "container_id": payload.container_id, "saved_to_db": True}
