347d085093b9b81d850c61324c9eed3579404884
ANKomyagin <144670657+ANKomyagin@users.noreply.github.com>
2026-03-22 14:47:19 +0300

    ghj

--- app/main.py
+++ app/main.py
@@ -2,9 +2,8 @@
 from fastapi import FastAPI
 from app.services.sensor_pipeline import SensorProcessingPipeline
 from app.infrastructure.routing.yandex_router import DummyRoutingProvider
-# from app.infrastructure.telegram.notifier import TelegramNotifier  # Пока отключаем
+from app.infrastructure.telegram.notifier import TelegramNotifier # Возвращаем
 from app.domain.models import WebhookPayload
-from app.core.interfaces import NotificationService # Добавили импорт интерфейса
 
 from app.infrastructure.database.database import engine, Base
 from app.infrastructure.database.postgres_repo import PostgresContainerRepo
@@ -15,23 +14,15 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Innopolis Smart Waste API")
 
 # ==========================================
-# 🛠 ЗАГЛУШКА ДЛЯ УВЕДОМЛЕНИЙ (Dummy)
-# ==========================================
-class DummyNotifier(NotificationService):
-    def send_alert(self, message: str, role: str):
-        # Вместо отправки реального сообщения в ТГ, просто красиво выводим в логи Docker
-        print(f"🔔 [DUMMY ALERT | Роль: {role}] -> {message}")
-
-# ==========================================
 # 🧱 СБОРКА ПАЙПЛАЙНА
 # ==========================================
 db_repo = PostgresContainerRepo()
-notifier = DummyNotifier() # ИСПОЛЬЗУЕМ НАШУ ЗАГЛУШКУ
+notifier = TelegramNotifier() # Возвращаем
 routing_provider = DummyRoutingProvider()
 
 sensor_pipeline = SensorProcessingPipeline(
     repo=db_repo,
-    notifier=notifier,
+    notifier=notifier, # Передаем аргумент!
     enable_alerts=True
 )
 
@@ -48,7 +39,6 @@ async def get_optimal_route():
 
     route = routing_provider.build_route(origin="Депо", waypoints=to_collect)
     return {"route": route}
-
 
 @app.post("/api/sensors/webhook")
 async def receive_sensor_data(payload: WebhookPayload):
